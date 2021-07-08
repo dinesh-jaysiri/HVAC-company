2004c53191525ccb05ece9d2afd621fa0b1d28d6
Dinesh Jyasir <ywdinesh@gmail.com>
2021-07-08 11:50:36 +0530

    updated testimonial

--- js/main.js
+++ js/main.js
@@ -5,3 +5,35 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+//slider
+
+const next = document.querySelector(".next");
+const back = document.querySelector(".back");
+const slider = document.querySelector(".slider");
+const testimonials = document.querySelectorAll(".testimonial");
+
+let sectionIndex = 1;
+function nextslide() {
+  if (sectionIndex < 3) {
+    testimonials[sectionIndex].classList.add("testimonial--small");
+    testimonials[sectionIndex + 1].classList.remove("testimonial--small");
+  }
+
+  sectionIndex = sectionIndex < 3 ? sectionIndex + 1 : 3;
+  slider.style.transform = "translate(" + sectionIndex * -20 + "%)";
+}
+function backslide() {
+  console.log(sectionIndex);
+
+  sectionIndex = sectionIndex < 2 ? 1 : sectionIndex - 1;
+  slider.style.transform = "translate(" + sectionIndex * -20 + "%)";
+  if (sectionIndex >= 1) {
+    testimonials[sectionIndex + 1].classList.add("testimonial--small");
+    testimonials[sectionIndex].classList.remove("testimonial--small");
+  }
+}
+
+next.addEventListener("click", nextslide);
+
+back.addEventListener("click", backslide);
